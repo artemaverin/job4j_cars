b9595b18cd7f8f5efc0998ee8dcf98305f586a29
Artem Averin <artem-averin-91@mail.ru>
2024-02-18 17:24:30 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,27 @@
+package ru.job4j.cars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Entity
+@Table(name = "price_history")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriceHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private long before;
+    private long after;
+    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+}
